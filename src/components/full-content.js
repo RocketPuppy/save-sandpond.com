020e105f36c4f5ce1d290dc6f3e870d1e218a9e6
Daniel Wilson <dwilson@wilsonhardrock.com>
2018-07-25 21:51:06 -0400

    Full width content flexes down

--- src/components/full-content.js
+++ src/components/full-content.js
@@ -5,6 +5,7 @@ export default glamorous.div({
   height: "100vh",
   padding: "3rem",
   display: "flex",
+  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
   overflow: "hidden"
