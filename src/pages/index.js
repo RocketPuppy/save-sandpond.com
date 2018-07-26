4e1fcc4620a82a90f3794d91e6d02c9ae78cf6c1
Daniel Wilson <dwilson@wilsonhardrock.com>
2018-07-25 21:51:20 -0400

    Add basic copy on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import FullContent from "../components/full-content";
+import glamorous from "glamorous";
+
+const Title = glamorous.h1({
+  fontSize: "48px",
+  fontWeight: "bold",
+  textAlign: "center"
+});
 
 const IndexPage = () => (
   <FullContent>
-    <h1>Save Sand Pond</h1>
+    <Title>Save Sand&nbsp;Pond</Title>
+    <h2>No Storage Facility</h2>
+    <h3>Warwick, RI</h3>
   </FullContent>
 )
 
