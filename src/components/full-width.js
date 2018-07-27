e6b4d35cf96184dd37d13e9e71d7991bfe93d2d8
Daniel Wilson <dwilson@wilsonhardrock.com>
2018-07-26 20:27:23 -0400

    Limit horizontal size of content

--- src/components/full-width.js
+++ src/components/full-width.js
@@ -3,6 +3,9 @@ import glamorous from "glamorous";
 
 export default glamorous.div({
   padding: "3rem",
+  maxWidth: "900px",
+  marginLeft: "auto",
+  marginRight: "auto",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
