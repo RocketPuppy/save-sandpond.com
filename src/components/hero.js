a3eaec00469b94d5a23d6e3af64f17499c55b062
Daniel Wilson <dwilson@wilsonhardrock.com>
2018-07-26 20:30:55 -0400

    Add hero component

---
+++ src/components/hero.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import glamorous from "glamorous";
+import styles from "../utils/styles";
+
+export default glamorous.div({
+  alignSelf: "flex-start",
+  margin: `${styles.spacing.large} 0`
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Link from 'gatsby-link'
 import FullWidth from "../components/full-width";
 import Title from "../components/title";
+import Hero from "../components/hero";
 import glamorous from "glamorous";
 
 const IndexPage = () => (
   <FullWidth>
-    <Title>Save Sand&nbsp;Pond</Title>
-    <h2>No Storage Facility</h2>
-    <h3>Warwick, RI</h3>
+    <Hero>
+      <Title>Save Sand&nbsp;Pond</Title>
+      <h2>No Storage Facility</h2>
+      <h3>Warwick, RI</h3>
+    </Hero>
   </FullWidth>
 )
 
