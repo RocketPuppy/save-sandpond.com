062776a0c7d3bafdda70941c731b88a27544bfce
Daniel Wilson <dwilson@wilsonhardrock.com>
2018-07-26 20:13:35 -0400

    Set up typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,40 @@
 import Typography from 'typography';
 
 const typography = new Typography({
-  baseFontSize: "18px"
+  baseFontSize: "24px",
+  baseLineHeight: 1.5,
+  scaleRatio: 2.2,
+  googleFonts: [
+    {
+      name: "Julius Sans One",
+      styles: ["400"]
+    },
+    {
+      name: "Cormorant Garamond",
+      styles: ["400", "700"]
+    },
+    {
+      name: "Carrois Gothic SC",
+      styles: ["400"]
+    }
+  ],
+  headerFontFamily: ["Julius Sans One", "sans-serif"],
+  bodyFontFamily: ["Cormorant Garamond", "serif"],
+  headerColor: "inherit",
+  bodyColor: "hsl(0, 0%, 0%, 0.8)",
+  headerWeight: "400",
+  bodyWeight: "400",
+  boldWeight: "700",
+  blockMarginBottom: "1rem",
+  includeNormalize: true
 });
+
+export const constants = {
+  fontFamilies: {
+    singleHeader: "Carrois Gothic SC",
+    multiHeader: "Julius Sans One",
+    body: "Cormorant Garamond"
+  }
+};
 
 export default typography;
