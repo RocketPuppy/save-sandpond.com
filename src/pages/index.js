26abcc7ed3996855f8f4d223ec11139ee617ef9d
Daniel Wilson-Thomas <daniel@mojotech.com>
2018-09-03 14:50:17 -0400

    Add paypal donate button

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,6 +65,15 @@ const IndexPage = ({ data }) => (
       header={<span>Help Save Sand&nbsp;Pond</span>}
       items={[
         (<span>Email us at: <a href="mailto:savesandpond@gmail.com">savesandpond@gmail.com</a></span>),
+        (<div>
+          <span>Donate:</span>
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick"/ >
+            <input type="hidden" name="hosted_button_id" value="J5A533UY4WRBE" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          </form>
+        </div>),
         (<div><span>Join our mailing list:</span><MailingListSignup /></div>)
       ]}
     />
